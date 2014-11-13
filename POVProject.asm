c3faf739373fbdd22a8ed583cd4240f7b2af634b
Juan Zaragoza <juan.zaragoza@teracode.com>
2014-11-13 12:23:42 -0300

    Agregado retardo dentro del .asm principal

--- POVProject.asm
+++ POVProject.asm
@@ -18,7 +18,7 @@
 MAIN:	SET_STACK ;inicializo stack
 CONFIG:	LDI R16,0xFF
 		OUT DDRA,R16 ;configuramos el puerto A como salida -> leds
-
+		
 		LDI	R16,0x02 ;configuramos el mcu control register para que active la interrupción en los dos flancos
 		OUT MCUCR,R16
 
@@ -30,16 +30,46 @@ CONFIG:	LDI R16,0xFF
 		SEI ;activamos las interrupciones globales
 
 HERE:	JMP HERE
-;HERE:	JMP INT0_ENCENDER_LEDS
+;	JMP INT0_ENCENDER_LEDS
 ;		JMP HERE
+
+RETARDO:
+	PUSH R21
+	PUSH R22
+	.DEF N = R20 ; "N" RETARDOS DE 200uS
+	.DEF A = R21 ; VARIABLE UTILIZADA PARA VARIOS PROPOSITOS
+	.DEF C = R22 ;VARIABLE UTILIZADA COMO CONTADOR DE OVERFLOWS
+	LDI C,0
+	LDI A,0B0000001
+	OUT TCCR0,A ; SETEO CLOCK EN MODO NORMAL Y CON PRESCALADO
+
+PRE_LOOP:
+    LDI A,1
+	OUT TIFR,A ; LIMPIO EL REGISTRO DE INTERRUPCIONES PARA UNA NUEVA LECTURA
+	LDI A,87
+	OUT TCNT0,A ; ABRO EL TIMER EN 0      
+
+LOOP:
+	IN A,TIFR ; OBTENGO EL VALOR DEL REGISTRO DE INTERRUPCIONES	
+    SBRS A,0 ; SALTEA LA SIGUIENTE INSTRICCION SI EL BIT 0 (TOV0) ES UN 1
+	RJMP LOOP ; BUCLE HASTA GENERAR 1 OVERFLOW (CONTAR DE 155 A 255 + 1) 
+	INC C ; INCREMENTO EL VALOR DEL CONTADOR
+	CP C,N
+	BRNE PRE_LOOP ; VUELVO AL LOOP SIN EL CONTADOR NO LLEGO A N.
+
+	POP R22
+	POP R21
+	RET
+
 
 INT0_ENCENDER_LEDS: ;interrupcion que se encarga de enceder leds
 
 		PUSH R16
 		PUSH R17
+		PUSH R20
 
-		LDI ZH,HIGH(FI_TEST)
-		LDI ZL,LOW(FI_TEST) ;inicializamos los punteros al principio de la tabla
+		LDI ZH,HIGH(FI_TEST<<1)
+		LDI ZL,LOW(FI_TEST<<1) ;inicializamos los punteros al principio de la tabla
 
 LEER:	LPM R16,Z+ ;cargamos el valor al registro
 
@@ -51,17 +81,22 @@ LEER:	LPM R16,Z+ ;cargamos el valor al registro
 		BREQ RETRASO_ESPACIO ;si es un espacio, esperamos el tiempo de un espacio
 
 RETRASO_LEDS:
-		;retrasar tiempo requerido entre leds encendidos
+		LDI R20,255;retrasar tiempo requerido entre leds encendidos
+		CALL RETARDO
 		RJMP LEER
 
 RETRASO_ESPACIO:
-		;retrasar tiempor requerido entre letras
+		LDI R20,255 ;retrasar tiempor requerido entre letras
+		CALL RETARDO
 		RJMP LEER
 
 FIN_INT0:
+		CLR R20
+		OUT PORTA,R20
+		POP R20
 		POP R17
 		POP R16
 		RETI
 
-FI_TEST:
-		.DB 0xFF,0x88,0x88,0x88,0x80,0,0xBF,'N';F i
+.cseg
+FI_TEST: .DB 0xF1,0x88,0x88,0x88,0x80,0,0xBF,'N';F i
